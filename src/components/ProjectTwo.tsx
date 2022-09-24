ebc5e51e3792cf400393deac8fbf5f9efa4f3aad
Diana P <dian6210@stud.kea.dk>
2022-09-25 00:12:38 +0200

    second project description added

--- src/components/ProjectTwo.tsx
+++ src/components/ProjectTwo.tsx
@@ -38,67 +38,43 @@ const ProjectTwo: React.FC = () => {
         </button>
       </header>
       <main>
-        <h1>Internship</h1>
+        <h1>Internship project</h1>
         <h2>Company</h2>
-        <p>MMMMM</p>
+        <p>
+          <span>MakeImpact - FinTech startup.</span> A mobile application where young people can learn about investments, connect with
+          peers, find companies that align with their personal values based on the 17 UN Sustainable Development Goals, read news about
+          these companies, and invest in them.
+        </p>
+        <h2>Internship period</h2>
+        <p>10 weeks</p>
         <h2>My role</h2>
         <p>Front-End Developer</p>
         <h2>Team</h2>
-        <p>MMMMMMM</p>
-        <h2>App features</h2>
+        <p>The product team consisted of 9 people: 2 designers, 2 front-end developers, 4 back-end developers, and a project manager.</p>
+        <h2>Tools and technologies</h2>
         <ul>
-          <li>Creating an account</li>
-          <li>Choosing SDGs that match the user's values</li>
-          <li>Logging in using email and password</li>
-          <li>Resetting password</li>
-          <li>Updating previously selected SDGs</li>
-          <li>Checking the list with matched companies</li>
-          <li>Searching for a specific company</li>
-          <li>Viewing detailed info about each company</li>
-          <li>Saving companies to watchlist</li>
-          <li>Buying and selling stocks</li>
-          <li>Seeing transactions in a portfolio and contribution to SDGs</li>
+          <li>
+            <span>Languages:</span> TypeScript, JavaScript
+          </li>
+          <li>
+            <span>Technologies:</span> React, Redux, SCSS, REST API, npm
+          </li>
+          <li>
+            <span>Source control:</span> Git
+          </li>
+          <li>
+            <span>Database:</span> Firebase
+          </li>
+          <li>
+            <span>Methodology:</span> Agile Software Development using SCRUM
+          </li>
+          <li>
+            <span>Communication:</span> Slack, Whereby
+          </li>
+          <li>
+            <span>Tools:</span> GitHub, Jira, Zeplin, Postman, BrowserStack, Google Lighthouse
+          </li>
         </ul>
-        <h2>Development process</h2>
-        <p>
-          The main methodology that I opted to work with during this project was <span>Design Thinking</span>, since it helps to provide a
-          better user experience. Design Thinking is broken up into five stages, these are: Empathize, Define, Ideate, Prototype, and Test.
-        </p>
-        <p>
-          During the Empathize phase, I did <span>desk research</span> to gain insights into the investment industry and conducted a{" "}
-          <span>survey</span> to get to know my target audience and their needs better. In order to get an overview of a product from a
-          business perspective I made sure to create <span>Business Model Canvas</span>. I did <span>design research</span> to understand
-          the trends and good practices used by competitors within the market.
-        </p>
-        <p>
-          During the Define phase, I wrote a <span>problem formulation</span> to set a clear goal for my project. I built a{" "}
-          <span>flow chart</span> as a visual representation of the possible flow a user could follow. Then I wrote{" "}
-          <span>user stories</span> to have a better understanding of why users need a certain functionality in the app and later I used
-          them for splitting the coding process into smaller tasks.
-        </p>
-        <p>
-          During the Ideate phase, I drew <span>Crazy 8's</span> and then final sketches.
-        </p>
-        <p>
-          During the Prototype phase, I made a high-fidelity prototype in <span>Adobe XD</span> and created icons and charts for the app
-          using <span>Adobe Illustrator</span>.
-        </p>
-        <p>
-          During the Testing phase, I collected insights from users by conducting <span>think aloud</span> and <span>5-second tests</span>.
-        </p>
-        <p>
-          The stack of technologies I used while coding includes <span>Sass</span>, <span>React</span>, and <span>Typescript</span>. I
-          worked with <span>git</span> and <span>GitHub</span> to both keep a remote backup of the files and to document progress in
-          general. For user authentication in my app I took advantage of the <span>Auth</span> service from <span>Firebase</span>. For
-          storing data that I collected from users during onboarding process and the companies that I created I put into the service{" "}
-          <span>Firestore database</span>.
-        </p>
-        <p>
-          To improve user experience by applying smooth transitions in the app and providing nice input fields I used{" "}
-          <span>npm packages</span>. To make sure that my solution looks how it should on different devices and browsers I utilized{" "}
-          <span>BrowserStack</span> service. I also used Google <span>Lighthouse</span> audit to check and improve the performance of the
-          app.
-        </p>
         <h2>Some screens that I have built</h2>
         <div className="projectImages">
           <img src={One} alt="App screen" />
